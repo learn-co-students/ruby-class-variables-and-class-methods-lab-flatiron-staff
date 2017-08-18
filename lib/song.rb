7c9d26faca1d4db1e122591c0221751ecc925c00
Haoshuang Wang <haoshuang@flatironschool.com>
2017-08-18 16:40:53 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @@genre_count
+    @@genres.each do |key|
+      if @@genre_count[key]
+         @@genre_count += 1
+      else
+        @@genre_count[key] = 1
+      end
+    end
+      @@genre_count
+  end
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+end
